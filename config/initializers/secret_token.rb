9ae96e9c930c63b82ea4afc68eb2d2df7112cbf2
Chris Roos <chris@seagul.co.uk>
2013-08-03 16:51:40 +0100

    Empty Rails 4.0.0 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThamesTimeLapse::Application.config.secret_key_base = 'ce910b92a9b564fa1530b7b9726b5f2d0dfe0626681c37106b686fdb5382849006bfb890a996ebe23841cd97fbc4cb75dc8e260808523b5ea7343e9fe3113c11'
