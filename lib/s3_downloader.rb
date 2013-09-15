e4d67d925a5d58788901f7e144af254824dcce88
Chris Roos <chris@seagul.co.uk>
2013-09-15 15:06:55 +0100

    Rename the images on S3
    Storing the images in date-specific folders on S3 make them easier to
    manage through the AWS console and 3Hub.

--- lib/s3_downloader.rb
+++ lib/s3_downloader.rb
@@ -13,7 +13,11 @@ class S3Downloader
           if s3_key =~ /imageSequence/
             image = Image.find_or_initialize_by(s3_key: s3_key)
             taken_at = DateTime.parse(s3_key[/imageSequence_(\d+).jpg/, 1])
-            image.update_attributes!(url: object.url, taken_at: taken_at)
+            new_key = "#{taken_at.to_date}/#{taken_at.strftime('%Y-%m-%d-%H-%M-%S')}.jpg"
+            if new_object = object.copy(key: new_key)
+              object.destroy
+            end
+            image.update_attributes!(url: new_object.url, taken_at: taken_at)
           end
         end
       end
