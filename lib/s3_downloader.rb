5bec29cf5d5de0ec61e117c9e097a3564a3085ed
Chris Roos <chris@seagul.co.uk>
2013-09-15 16:12:29 +0100

    Split the tasks for moving and storing image info
    I've changed the FolderSync settings to upload to the '_uploads' folder
    on S3, and split the tasks to move the files and then separately
    download the image information. Prior to this, I was only recording the
    image information if I also moved the image.
    
    I'm living with the duplication for now but plan to refactor later.

--- lib/s3_downloader.rb
+++ lib/s3_downloader.rb
@@ -1,6 +1,6 @@
 class S3Downloader
   class << self
-    def download
+    def move_images
       aws_s3_connection_attrs = {
         access_key_id: AWS_ACCESS_KEY_ID.to_s,
         secret_access_key: AWS_SECRET_ACCESS_KEY.to_s
@@ -10,14 +10,35 @@ class S3Downloader
       if bucket = service.buckets.find('thames-time-lapse')
         bucket.objects.each do |object|
           s3_key = object.key
-          if s3_key =~ /imageSequence/
-            image = Image.find_or_initialize_by(s3_key: s3_key)
+          if s3_key =~ /_uploads.*\.jpg/
             taken_at = DateTime.parse(s3_key[/imageSequence_(\d+).jpg/, 1])
             new_key = "#{taken_at.to_date}/#{taken_at.strftime('%Y-%m-%d-%H-%M-%S')}.jpg"
+            puts "Moving from #{s3_key} to #{new_key}"
             if new_object = object.copy(key: new_key)
               object.destroy
             end
-            image.update_attributes!(url: new_object.url, taken_at: taken_at)
+          end
+        end
+      end
+    end
+
+    def record_image_information
+      aws_s3_connection_attrs = {
+        access_key_id: AWS_ACCESS_KEY_ID.to_s,
+        secret_access_key: AWS_SECRET_ACCESS_KEY.to_s
+      }
+
+      service = S3::Service.new(aws_s3_connection_attrs)
+      if bucket = service.buckets.find('thames-time-lapse')
+        bucket.objects.each do |object|
+          s3_key = object.key
+          next if s3_key =~ /_uploads/
+          if s3_key =~ /(\d{4}-\d{2}-\d{2})-(\d{2})-(\d{2})-(\d{2})\.jpg/
+            date = $1
+            hour, minute, second = $2, $3, $4
+            image = Image.find_or_initialize_by(s3_key: s3_key)
+            taken_at = DateTime.parse("#{date} #{hour}:#{minute}:#{second}")
+            image.update_attributes!(url: object.url, taken_at: taken_at)
           end
         end
       end
--- lib/tasks/s3.rake
+++ lib/tasks/s3.rake
@@ -1,7 +1,10 @@
 require 's3_downloader'
 
 namespace :s3 do
-  task :download_image_information => :environment do
-    S3Downloader.download
+  task :move_images => :environment do
+    S3Downloader.move_images
+  end
+  task :record_image_information => :environment do
+    S3Downloader.record_image_information
   end
 end
