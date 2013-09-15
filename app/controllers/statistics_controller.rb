2358aafaa32360631e3a155db2dc95229607abbc
Chris Roos <chris@seagul.co.uk>
2013-09-15 17:39:13 +0100

    Order the stats by date

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,5 +1,5 @@
 class StatisticsController < ApplicationController
   def index
-    @image_stats = Image.group("DATE(taken_at)").count
+    @image_stats = Image.group("DATE(taken_at)").order('DATE(taken_at) DESC').count
   end
 end
