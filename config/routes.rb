b2b58eeb0ff56f9337e2992c1a7636beb141c0bc
Chris Roos <chris@seagul.co.uk>
2013-10-05 12:50:46 +0100

    Route to the current day by default
    I think this is better than showing /images by default.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ThamesTimeLapse::Application.routes.draw do
-  root 'images#index'
+  root 'days#index'
   resources :images, only: [:index, :show]
   resources :days, only: [:index, :show]
   get '/stats', to: 'statistics#index'
