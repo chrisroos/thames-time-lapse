2ce8988d7a36935dec572e8abe4ba705acd8aa66
Chris Roos <chris@seagul.co.uk>
2014-08-25 20:34:22 +0100

    Add script to create and upload timeslice images

---
+++ script/create-and-upload-timeslice-for-yesterday.rb
@@ -0,0 +1,27 @@
+# This expects to find images in Rails.root + /yyyy-mm-dd/800x600/
+
+def log(string)
+  puts "(#{File.basename(__FILE__)}) #{string}"
+end
+
+def run(cmd)
+  log cmd
+  `#{cmd}`
+end
+
+started_at = Time.now
+log "Started at: #{started_at}"
+
+date = Date.yesterday.to_s
+size = '800x600'
+
+# Create the timeslice photo for yesterday
+run "ruby script/create-time-slice-photo.rb ./#{date}/#{size}"
+
+# Upload the timeslice to S3
+run %%s3cmd put ./#{date}/#{size}/_time-slices/#{date}-time-slice.jpg s3://thames-time-lapse/images/#{date}/%
+
+finished_at = Time.now
+log "Finished at: #{finished_at}"
+
+log "Duration: #{finished_at - started_at} seconds"
